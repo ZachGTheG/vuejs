ad2f6151a4868bd6fbebff058d1e823cb12ef155
Zach <113333499+ZachGTheG@users.noreply.github.com>
2023-02-09 13:21:28 -0500

    changes
    changes

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div class="button">
+    <button class="duplicator" v-on:click="duplicator">Duplicator</button>
+    <button class="toggle-background">Toggle Background</button>
+    <button class="change-heading">Change Heading</button>
+    <button class="delete-card">Delete Card</button>
+  </div>
+  <div id="card" class="card">
+    <h1>The Penn State Logo</h1>
+    <img src="https://www.psu.edu/psu-edu-assets/images/shared/psu-mark.svg">
+    <p>This is the logo that The Pennsylvania State University uses.</p>
+    <button class="details">Details</button>
+  </div>
 </template>
 
 <script setup>
@@ -8,6 +18,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
+
 </script>
 
 <style>
@@ -18,5 +29,70 @@ import HelloWorld from './components/HelloWorld.vue'
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.duplicator:hover {
+  background-color: green;
+}
+
+.duplicator:focus {
+  background-color: green;
+}
+
+.card {
+  width: 350px;
+  border: 2px solid black;
+  text-align: center;
+}
+
+h1 {
+  margin-top: 8px;
+  margin-left: 32px;
+  margin-right: 32px;
+}
+
+img {
+  display: block;
+  margin-left: 120px;
+  margin-right: auto;
+  margin-top: 16px;
+  width: 400px;
+}
+
+p {
+  margin-top: 16px;
+  margin-bottom: 32px;
+  display: none;
+}
+
+.details {
+  margin-bottom: 8px;
+  border: 2px solid black;
+  background-color: gray;
+  color: blue;
+}
+
+@media only screen and (min-width: 500px) and (max-width: 800px) {
+  button {
+    display: none;
+  }
+}
+
+@media only screen and (max-width: 500px) {
+  div {
+    transform: scale(0.8);
+  }
+
+  img {
+    width: 80%;
+  }
+}
+
+.bg-toggled {
+  background-color: green;
+}
+
+.hidden {
+  display: block;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -24,6 +24,55 @@ defineProps({
 })
 
 const state = reactive({ count: 0 })
+
+
+
+duplicator()
+{
+  document.querySelector(".duplicator").addEventListener("click", function (e) {
+    const node = document.getElementById("card");
+    let clone = node.cloneNode(true);
+
+    document.body.appendChild(clone);
+  });
+}
+
+  const button = document.querySelector(".details");
+  button.addEventListener("mouseenter", function () {
+    button.style.backgroundColor = "green";
+  });
+  button.addEventListener("mouseleave", function () {
+    button.style.backgroundColor = "";
+  });
+
+  const card = document.querySelector("#card");
+  const toggleBackgroundButton = document.querySelector(".toggle-background");
+
+  toggleBackgroundButton.addEventListener("click", function () {
+    card.classList.toggle("bg-toggled");
+  });
+
+  const heading = document.querySelector("#card h1");
+  const changeHeadingButton = document.querySelector(".change-heading");
+
+  changeHeadingButton.addEventListener("click", function () {
+    heading.textContent = "Something Else";
+  });
+
+  const cardContainer = document.querySelector("#card").parentNode;
+  const deleteCardButton = document.querySelector(".delete-card");
+
+  deleteCardButton.addEventListener("click", function () {
+    cardContainer.removeChild(cardContainer.lastChild);
+  });
+
+  const description = document.querySelector("#card p");
+  const detailsButton = document.querySelector(".details");
+
+  detailsButton.addEventListener("click", function () {
+    description.classList.toggle("hidden");
+  });
+
 </script>
 
 <style scoped>
